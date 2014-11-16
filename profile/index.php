fb4599b1e9638b568d5b85eac5073f24082eebd9
Pankamol Srikaew <aim.ske09@gmail.com>
2014-11-17 03:53:52 +0900

    task, profile webpage added

--- profile/index.php
+++ profile/index.php
@@ -12,17 +12,17 @@
 
 
 		<!-- frontpage css -->
-		<link href="css/frontpage.css" rel="stylesheet">
+		<link href="../css/frontpage.css" rel="stylesheet">
 
 		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/bootstrap-theme.min.css" rel="stylesheet">
 
 
 		<!-- Push & slide css -->
-		<link rel="stylesheet" type="text/css" href="css/default.css" />
-		<link rel="stylesheet" type="text/css" href="css/component.css" />
-		<script src="js/modernizr.custom.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/default.css" />
+		<link rel="stylesheet" type="text/css" href="../css/component.css" />
+		<script src="../js/modernizr.custom.js"></script>
 	</head>
 
 	<body class="cbp-spmenu-push">
@@ -39,30 +39,13 @@
 		<div class="container">
 			<header class="clearfix">
 				<span>E-Government</span>
-				<h1>Working Area</h1>
+				<h1>Your Profile</h1>
 				<nav>
                     <div class="dropdown">
 					
 					<button class="noborder" id="showLeftPush" class="icon-drop" data-info="Menu"><span class="glyphicon glyphicon-align-justify bigfont" aria-hidden="true"></span></button>
-					<button class="noborder" id="showLeftPush" data-info="Login" data-toggle="dropdown"><span class="glyphicon glyphicon-user bigfont" aria-hidden="true"></span></button>
-                        <ul  class="dropdown-menu" style="padding: 10px; background: #ddd">
-                        <form action="" role="form">
-                        <div class="form-group">
-                        <label for="user">User</label>
-                        <input type="text" class="form-control" id="user" placeholder="User" />
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Password" />
-                        <div class="checkbox">
-  						  <label> <input type="checkbox"> Remember me</label>
-  						</div>
-                        </div>
-                        <button type="button" class="btn btn-primary maxwidth">Sign in</button>
-                        <button type="button" class="btn btn-warning maxwidth">Forgot Password ?</button>
-                        <button type="button" class="btn btn-success maxwidth">New Account</button>
-
-                        </form>
-                        </ul>
-                   
+					<button class="noborder" id="showLeftPush" data-info="Login" data-toggle="dropdown"><span class="glyphicon glyphicon-user bigfont" aria-hidden="true"></span>Hello, User</button>
+                    <button type="button" class="btn btn-danger noborder signout-btn">Sign out</button>
                     </div>
 				</nav>
 			</header>
@@ -83,7 +66,7 @@
 
 				<section id="fbsection5">
 					
-					<div class="container text-center">E-Government
+					<div class="container text-center">
 
 					<div class="container minibox"></div>
 
@@ -102,7 +85,7 @@
 		
 
 		<!-- Classie - class helper functions by @desandro https://github.com/desandro/classie -->
-		<script src="js/classie.js"></script>
+		<script src="../js/classie.js"></script>
 		<script>
 			var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
 				showLeftPush = document.getElementById( 'showLeftPush' ),
@@ -130,17 +113,17 @@
 
 		<!-- Bootstrap -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
 
         
 
 		<!-- jquery.easing by http://gsgd.co.uk/ : http://gsgd.co.uk/sandbox/jquery/easing/ -->
-		<script src="js/jquery.easing.min.js"></script>
+		<script src="../js/jquery.easing.min.js"></script>
 		<!-- waypoints jQuery plugin by http://imakewebthings.com/ : http://imakewebthings.com/jquery-waypoints/ -->
-		<script src="js/waypoints.min.js"></script>
+		<script src="../js/waypoints.min.js"></script>
 		<!-- jquery-smartresize by @louis_remi : https://github.com/louisremi/jquery-smartresize -->
-		<script src="js/jquery.debouncedresize.js"></script>
-		<script src="js/cbpFixedScrollLayout.js"></script>
+		<script src="../js/jquery.debouncedresize.js"></script>
+		<script src="../js/cbpFixedScrollLayout.js"></script>
 		<script>
 			$(function() {
 				cbpFixedScrollLayout.init();
