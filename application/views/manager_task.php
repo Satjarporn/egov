246304e9d3602e4fe264ae1e09adfbced86d7362
Pankamol Srikaew <aim.ske09@gmail.com>
2014-12-01 13:49:38 +0900

    bug fixed

--- application/views/manager_task.php
+++ application/views/manager_task.php
@@ -145,10 +145,10 @@
                    <div style="position:relative;">
         <a class='btn btn-primary' href='javascript:;'>
             Upload Task Description File
-            <input type="file" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;' name="file_source" size="40"  onchange='$("#upload-file-info").html($(this).val());'>
+            <input type="file" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;' name="file_source" size="40"  onchange='$("#new-task-upload-file-info").html($(this).val());'>
         </a>
         &nbsp;
-        <span class='label label-info' id="upload-file-info"></span>
+        <span class='label label-info' id="new-task-upload-file-info"></span>
       </div>
               </div>
             <div class="form-group">
